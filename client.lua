b2464ba232fe643a45b3328047bb99d9d8d2fa5a
Robert Masen <robert.masen@smartthings.com>
2021-07-06 16:02:44 -0500

    example updates

--- client.lua
+++ client.lua
@@ -4,10 +4,10 @@ local socket = require 'socket'
 local sock = socket.tcp()
 sock:connect('0.0.0.0', 8080)
 
-local r = assert(lunch.Request.new('GET', '/', lunch.utils.buffered_socket_sink(sock))
+local r = assert(lunch.Request.new('GET', '/', sock)
   :append_body('asdf'))
 
-for line in r:as_source() do
+for line in r:iter() do
   sock:send(line)
 end
 
